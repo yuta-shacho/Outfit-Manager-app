301e117ecf2f6112e3aa94013328507c4b46f229
yuta3268-shacho <yuta-soccer-3268@docomo.ne.jp>
2024-09-30 23:51:26 +0900

    月の日別の服代棒グラフコンポーネント（プライベートからパブリックへの変更によるリポジトリ変更）

---
+++ src/components/BarChart.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { calculateDailyExpense } from "../utils/financeCalculations";
+import { Transaction } from "../types";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Box, Typography } from "@mui/material";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface BarChartProps {
+  monthlyTransactions: Transaction[];
+  isLoading: boolean;
+}
+
+const BarChart = ({ monthlyTransactions, isLoading }: BarChartProps) => {
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "日別の服代",
+      },
+    },
+  };
+
+  const dailyExpense = calculateDailyExpense(monthlyTransactions);
+  console.log(dailyExpense);
+
+  const dateLabels = Object.keys(dailyExpense).sort();
+  console.log(dateLabels);
+
+  const expenseData = dateLabels.map((day) => dailyExpense[day].monthExpense);
+  console.log(expenseData);
+
+  const data = {
+    labels: dateLabels,
+    datasets: [
+      {
+        label: "服代",
+        data: expenseData,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {isLoading ? (
+        <CircularProgress />
+      ) : monthlyTransactions.length > 0 ? (
+        <Bar options={options} data={data} />
+      ) : (
+        <Typography>データがありません</Typography>
+      )}
+    </Box>
+  );
+};
+
+export default BarChart;
