1b2450efd176f4eaa20a808700fe9e8eef445d2a
yuta3268-shacho <yuta-soccer-3268@docomo.ne.jp>
2024-09-30 23:52:43 +0900

    カレンダーコンポーネント（プライベートからパブリックへの変更によるリポジトリ変更）

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,87 @@
+import FullCalendar from "@fullcalendar/react";
+import React from "react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import "../calendar.css";
+import { DatesSetArg, EventContentArg } from "@fullcalendar/core/index.js";
+import { calculateDailyExpense } from "../utils/financeCalculations";
+import { CalendarContent, Transaction } from "../types";
+import { formatCurrency } from "../utils/formatting";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import { isSameMonth } from "date-fns";
+
+interface CalendarProps {
+  monthlyTransactions: Transaction[];
+  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>;
+  setCurrentDay: React.Dispatch<React.SetStateAction<string>>;
+  currentDay: string;
+  today: string;
+  handleDateClick: (dateInfo: DateClickArg) => void;
+}
+const Calendar = ({
+  monthlyTransactions,
+  setCurrentMonth,
+  setCurrentDay,
+  currentDay,
+  today,
+  handleDateClick,
+}: CalendarProps) => {
+  const dailyExpense = calculateDailyExpense(monthlyTransactions);
+
+  //データをイベント用に生成しなおす関数
+  const createCalendarEvents = (
+    dailyExpense: Record<string, { monthExpense: number }>
+  ): CalendarContent[] => {
+    return Object.keys(dailyExpense).map((date) => {
+      const { monthExpense } = dailyExpense[date];
+      return {
+        start: date,
+        monthExpense: formatCurrency(monthExpense),
+      };
+    });
+  };
+
+  const backgroundEvent = {
+    start: currentDay,
+    display: "background",
+    backgroundColor: "#81d4fa",
+  };
+
+  const calendarEvents = createCalendarEvents(dailyExpense);
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    return (
+      <>
+        <div className="money" id="event-monthExpense">
+          {eventInfo.event.extendedProps.monthExpense}
+        </div>
+        <div className="money" id="event-yearExpense">
+          {eventInfo.event.extendedProps.yearExpense}
+        </div>
+      </>
+    );
+  };
+
+  const handleDateSet = (datesetInfo: DatesSetArg) => {
+    const currentMonth = datesetInfo.view.currentStart;
+    setCurrentMonth(currentMonth);
+    const todayDate = new Date();
+    if (isSameMonth(todayDate, currentMonth)) {
+      setCurrentDay(today);
+    }
+  };
+
+  return (
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      events={[...calendarEvents, backgroundEvent]}
+      eventContent={renderEventContent}
+      datesSet={handleDateSet}
+      dateClick={handleDateClick}
+    />
+  );
+};
+
+export default Calendar;
