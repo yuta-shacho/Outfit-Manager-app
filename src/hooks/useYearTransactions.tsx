6ae8cfe7a722d480e5e4570cad47a9c182040295
yuta3268-shacho <yuta-soccer-3268@docomo.ne.jp>
2024-10-01 04:00:18 +0900

    カスタムフックで年間の服代をグローバルに管理

---
+++ src/hooks/useYearTransactions.tsx
@@ -0,0 +1,17 @@
+import React, { useMemo } from "react";
+import { useAppContext } from "../context/AppContext";
+import { formatYear } from "../utils/formatting";
+import { Transaction } from "../types";
+
+const useYearTransactions = (): Transaction[] => {
+  const { transactions, currentMonth } = useAppContext();
+  //今年のデータをすべて取得(ここでエラーが出たらformatYearの引数にnew DateをStateで新しく定義して入れて)
+  const yearTransactions = useMemo(() => {
+    return transactions.filter((transaction) =>
+      transaction.date.startsWith(formatYear(currentMonth))
+    );
+  }, [transactions, currentMonth]);
+  return yearTransactions;
+};
+
+export default useYearTransactions;
