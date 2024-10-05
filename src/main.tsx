f5a2e012dbb938422e00509a9d5766a25d83695f
yuta3268-shacho <yuta-soccer-3268@docomo.ne.jp>
2024-10-06 07:49:49 +0900

    認証機能追加による細かい変更

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
+import { AppContextProvider } from "./context/AppContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </Provider>
+  </StrictMode>
+);
