442d7e9fb9909a8ad992c9850c54f84a40a46162
yuta3268-shacho <yuta-soccer-3268@docomo.ne.jp>
2024-10-06 07:33:08 +0900

    ユーザーログイン画面

--- src/pages/FirebaseAuth.tsx
+++ src/pages/FirebaseAuth.tsx
@@ -1,34 +1,106 @@
-import firebase from "firebase/compat/app"; // FirebaseUIのために互換バージョンを使用
-import "firebaseui/dist/firebaseui.css";
-import * as firebaseui from "firebaseui";
-import { auth } from "../firebase";
-import { useEffect } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { signInWithPopup } from "firebase/auth";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { auth, githubProvider, googleProvider } from "../firebase";
 
 const FirebaseAuth = () => {
-  useEffect(() => {
-    // FirebaseUIの設定
-    const uiConfig = {
-      signInOptions: [
-        {
-          provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID, // Google認証を設定
-          clientId:
-            "649897087042-65pvmd68dnmvik1tnpdqrlub9nt6i3u8.apps.googleusercontent.com",
-        },
-      ],
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, googleProvider).catch((err) => {
+      alert(err.message);
+    });
+  };
+  const signInWithGithub = () => {
+    signInWithPopup(auth, githubProvider).catch((err) => {
+      alert(err.message);
+    });
+  };
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        background:
+          "linear-gradient(to right, rgba(243, 196, 170, 1), rgba(162, 120, 182, 1), rgba(52, 59, 202, 1))",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Card
+        sx={{
+          padding: 4,
+          marginBottom: 3,
+          maxWidth: "lg",
+          boxShadow: 3,
+          bgcolor: "rgba(231, 227, 210, 255)",
+          borderRadius: 5,
+        }}
+      >
+        <Stack alignItems="center">
+          <img
+            src="../../public/rogo.webp"
+            alt=""
+            style={{ borderRadius: "50%", width: "80px" }}
+          />
 
-      signInSuccessUrl: "/home", // ログイン後にリダイレクトされるURL
-      signInFlow: "popup", // 認証方法をポップアップに設定
-    };
+          <Typography
+            variant="h4"
+            component="h1"
+            sx={{ fontFamily: "Nico Moji", paddingBottom: 3 }}
+          >
+            ふくログ!!
+          </Typography>
+          <Typography variant="h4">ログイン</Typography>
+          <Typography
+            variant="body2"
+            sx={{ marginTop: 2, textAlign: "center" }}
+          >
+            Please sign in with your Google or GitHub account！
+          </Typography>
+        </Stack>
 
-    // FirebaseUIのインスタンスを取得し、ない場合は新規作成
-    const ui =
-      firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
-    ui.start("#firebaseui-auth-container", uiConfig);
-
-    return () => ui.reset(); // コンポーネントがアンマウントされるときにUIをリセット
-  }, []);
-
-  return <div id="firebaseui-auth-container"></div>;
+        <Divider sx={{ borderColor: "gray.400" }} />
+        <CardContent>
+          <Stack spacing={2}>
+            <Button
+              onClick={signInWithGoogle}
+              variant="outlined"
+              startIcon={<FcGoogle size="30px" />}
+              sx={{
+                fontSize: "20px",
+                padding: 2,
+                borderColor: "gray.400",
+                textTransform: "none",
+              }}
+            >
+              Googleでログイン
+            </Button>
+            <Button
+              onClick={signInWithGithub}
+              variant="outlined"
+              startIcon={<FaGithub size="30px" />}
+              sx={{
+                fontSize: "20px",
+                padding: 2,
+                borderColor: "gray.400",
+                textTransform: "none",
+              }}
+            >
+              Githubでログイン
+            </Button>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 };
 
 export default FirebaseAuth;
